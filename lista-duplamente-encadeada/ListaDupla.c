eff37d3920acd40e27a88fa459cab01ffcb9e35e
FrancoFlores <francobarbosaflores@gmail.com>
2023-09-15 17:38:06 -0300

    criação da função para criar um nó

--- lista-duplamente-encadeada/ListaDupla.c
+++ lista-duplamente-encadeada/ListaDupla.c
@@ -6,3 +6,14 @@ typedef struct no {
     struct no *ant;
 }No;
 
+No *criaNo(int valor){
+No *novo = (No*)malloc(sizeof(No));
+if(novo == NULL){
+    printf("Erro de alocação de memória! \n");
+    exit(1);
+}
+novo->chave = valor;
+novo->prox = NULL;
+novo->ant = NULL;
+return novo;
+}
