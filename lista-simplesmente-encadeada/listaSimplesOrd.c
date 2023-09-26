6e5e74005aafe1c72c640cdd0953039d48f129a9
FrancoFlores <francobarbosaflores@gmail.com>
2023-09-26 12:12:25 -0300

    função de para inserir em uma liste ordenada para que ela continue ordenada

--- lista-simplesmente-encadeada/listaSimplesOrd.c
+++ lista-simplesmente-encadeada/listaSimplesOrd.c
@@ -6,6 +6,42 @@ typedef struct no{
     struct no *prox;
 }No;
 
+No *criaNo(int valor){
+    No *novo = (No*)malloc(sizeof(No));
+    if (novo == NULL){
+        printf("Erro de alocação de memória!");
+        exit(1);
+    }
+    novo->chave = valor;
+    novo->prox = NULL;
+    return novo;
+}
+
+No *insereOrd(No *L, int valor){
+    No *novo = criaNo(valor);
+    No *aux = L;
+    No *pred = NULL;
+
+    if(L == NULL)
+        L = novo;
+    else{
+        while(aux != NULL && valor > aux->chave){
+            pred = aux;
+            aux = aux->prox;
+        }
+        
+        //inserir no inicio
+        if(pred == NULL){ 
+            novo->prox = aux;
+            L = novo;
+        }else{
+        //insere no meio ou no final
+            novo->prox = aux;
+            pred->prox = novo;
+        }
+    }
+    return L;
+}
 int main(){
     return 0;
 }
